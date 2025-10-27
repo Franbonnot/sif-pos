2bac9086bde6e532f6c44ebad48da5ee418acf6d
Franbonnot <usafranbonnot@gmail.com>
2025-10-27 03:55:06 -0300

    Configuro db.js y server.js para Render y local

--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
+// src/db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined
+  ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params)
-};
+module.exports = pool;
