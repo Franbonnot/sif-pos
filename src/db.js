5ff292c22f69e5b66b5e09981c4ab360740cce19
Franbonnot <usafranbonnot@gmail.com>
2025-10-28 00:18:50 -0300

    Corrijo conexión a PostgreSQL con DATABASE_URL

--- src/db.js
+++ src/db.js
@@ -1,10 +1,9 @@
-// src/db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false } // Render necesita SSL
+    ? { rejectUnauthorized: false }
     : false
 });
 
